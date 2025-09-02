cd39e2331caab110f95741ac84921bf9b2aa229c
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-09-02 19:12:14 +0530

    implement mongodb connection config

--- server/server.js
+++ server/server.js
@@ -5,5 +5,12 @@ const dotenv = require("dotenv");
 dotenv.config({ quiet: true });
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URI = process.env.MONGO_URI;
+
+const connectToMongoDb = require("./config/connection");
+
+connectToMongoDb(MONGO_URI)
+  .then(() => console.log("Connect mongoDB"))
+  .catch((err) => console.log("mongoDB connection failed", err));
 
 app.listen(PORT, () => console.log(`Server running at PORT:${PORT}`));
