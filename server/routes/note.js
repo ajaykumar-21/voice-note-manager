befb352a63733ac3d01b564be0ff57104ec664fc
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-09-04 00:12:10 +0530

    Added fs + path logic to automatically create 'uploads' folder if missing

--- server/routes/note.js
+++ server/routes/note.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
 const {
   getNotes,
@@ -10,13 +12,19 @@ const {
   summarizeNote,
 } = require("../controllers/note");
 
+const uploadDir = path.join(process.cwd(), "uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
 // simple disk storage for audio
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
-    cb(null, `${Date.now()} - ${file.originalname}`);
+    cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
